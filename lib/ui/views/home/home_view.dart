5861c2546b2380c7825f55da19d68af970409f56
krinit <krishkc2084@gmail.com>
2025-03-26 22:37:44 +0530

    Home Screen First Draft

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -11,60 +11,104 @@ class HomeView extends StackedView<HomeViewModel> {
   @override
   Widget builder(BuildContext context, HomeViewModel viewModel, Widget? child) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Center(
+      appBar: AppBar(
+          title: Text('Catalogue'),
+        backgroundColor: appBarColor,
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: (){}, icon: Icon(Icons.shopping_cart)
+          )
+        ],
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 0.5,
+        ),
+        itemCount: viewModel.products.length,
+        itemBuilder: (context, index) {
+          final product = viewModel.products[index];
+          double discount = product.discountPercentage;
+          String discountedPrice = (product.price *(1-discount/100)).toStringAsFixed(2);
+          return Card(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            elevation: 4,
             child: Column(
-              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                verticalSpaceLarge,
-                Column(
+                Stack(
                   children: [
-                    Text(
-                      '${viewModel.products.length}',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
+                    ClipRRect(
+                      borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                      child: Image.network(
+                        product.thumbnail,
+                        fit: BoxFit.cover,
+                        height: 200,
+                        width: double.infinity,
                       ),
                     ),
-                    verticalSpaceMedium,
-                    MaterialButton(
-                      color: Colors.black,
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(color: Colors.white),
+                    Positioned(
+                      bottom: 0,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed: () {
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.pinkAccent,
+                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          elevation: 4,
+                        ),
+                        child: Text("Add"),
                       ),
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showDialog,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(color: Colors.white),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(product.title, style: const TextStyle(fontWeight: FontWeight.bold)),
+                      Text(product.brand ?? 'Generic', style: const TextStyle(color: Colors.grey)),
+                      verticalSpaceSmall,
+                      Row(
+                        children: [
+                          Text(
+                            '₹$discountedPrice',
+                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          horizontalSpaceSmall,
+                          Text(
+                            '₹${product.price.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showBottomSheet,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(color: Colors.white),
+                      const SizedBox(height: 4),
+                      Text(
+                        '${discount.toStringAsFixed(2)}% OFF',
+                        style: const TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
